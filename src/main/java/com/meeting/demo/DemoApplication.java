4334a645cdbb50a6a5c3b1028511965dcb9ac962
syf123456 <1169662582@qq.com>
2021-06-04 00:18:29 +0800

    bug修改

--- src/main/java/com/meeting/demo/DemoApplication.java
+++ src/main/java/com/meeting/demo/DemoApplication.java
@@ -1,12 +1,8 @@
 package com.meeting.demo;
 
-import org.apache.catalina.connector.Connector;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @MapperScan("com.meeting.demo.mapper")
@@ -16,16 +12,16 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
     }
 
-    @Bean
-    public ServletWebServerFactory servletContainer() {
-        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
-        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
-        return tomcat;
-    }
-
-    private Connector createStandardConnector() {
-        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-        connector.setPort(8080);
-        return connector;
-    }
+//    @Bean
+//    public ServletWebServerFactory servletContainer() {
+//        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+//        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+//        return tomcat;
+//    }
+//
+//    private Connector createStandardConnector() {
+//        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+//        connector.setPort(8080);
+//        return connector;
+//    }
 }
--- src/main/java/com/meeting/demo/api/FeedbackApi.java
+++ src/main/java/com/meeting/demo/api/FeedbackApi.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
@@ -19,6 +20,7 @@ public class FeedbackApi {
 
     @RequestMapping("/insert")
     public void insert(Feedback feedback) {
+        HashMap hashMap = new HashMap();
         feedbackService.insert(feedback);
     }
 
